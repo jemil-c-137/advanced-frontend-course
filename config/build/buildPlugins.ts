988cab75ce01fadeaca811d5083a3b2af5014ff9
jemil-c-137 <jemil1931@gmail.com>
2024-03-12 22:52:24 +0100

    remove analizer from prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,14 +21,16 @@ export function buildPlugins({
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
     ];
 
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(new ReactRefreshWebpackPlugin());
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
+        );
     }
 
     return plugins;
